736e296396966f420ed5263c8880885db015c2e4
Jason Stone <jason.stone0808@gmail.com>
2018-12-03 18:23:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.collect { |name, index|
+  attendees.each.with_index.collect { |name, index|
   "Hello, #{name}! You'll be assigned to room #{index+1}!"}
 end
 
